695d022ed9f47408e6b2399b47b208e84e51fb3e
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:00 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,7 @@
+name=input("Enter the student name")
+roll_number=int(input("Roll number of the student:"))
+roll_number=roll_number+1
+cgpa=float(input("Enter your cgpa:"))
+percentage_marks=cgpa*10
+print(percentage_marks)
+
