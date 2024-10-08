aaf7f18b158da8cdc1fa9ec0ef57389c499665ed
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-10-08 15:11:48 +0530

    Add files via upload

---
+++ arithmetic operators.py
@@ -0,0 +1,30 @@
+'''
+author:NIKKITA ANNA GEORGE
+created on:8/10?24
+program using arithmetic operators.
+'''
+
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+number3=int(input("Enter the third number:"))
+sum=(number1+number2)
+print(sum)
+print("The sum of num1 and num2 is:",sum)
+
+number1=int(input("Enter the first number:"))
+number2=int(input("enter the second number:"))
+number3=int(input("enter the third number"))
+subtract=(number1-number2)
+print(subtract)
+print("The difference when num2 is subtracted from num1 is:",subtract)
+
+product=(number1*number2)
+print("The product of num1 and num2 is:",product)
+
+division=(number1/number2)
+print("The quotient when num1 is divided by num2 is:",division)
+
+modulus=(number1%number2)
+print("The remainder when num1 is divided by num2 is:",modulus)
+combined=(number1 + number2) * number3 / 2
+print("The result of (num1 + num2) * num3 / 2 is:",combined)
--- string.py
+++ string.py
@@ -1,4 +1,13 @@
-first_name="jane"
-last_name="doe"
-name=first_name+" "+last_name
-print(name)
+"""
+author:Nikkita Anna George
+created on:8/10/24
+program for Concatenate string.
+"""
+first_name=input("Enter your first name")
+second_name=input("Enter your second name")
+full_name=first_name+" "+second_name
+print(full_name)
+length=len(first_name)
+print(length)
+extracted_second_name=full_name[:length]
+print(extracted_second_name)
