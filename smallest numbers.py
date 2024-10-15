f80aab550467f2e4e7d33a88ee48590c4aaab7b0
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-10-15 14:55:19 +0530

    Add files via upload

---
+++ smallest numbers.py
@@ -0,0 +1,11 @@
+
+
+number1=int(input("Enter your first number:"))
+number2=int(input("Enter your second number:"))
+number3=int(input("Enter your third number:"))
+if number1<number2 and number1<number3:
+    print("The smallest number is:",number1)
+elif number2<number1 and number2<number3:
+    print("The smallest number is:", number2)
+else:
+    print("The smallest number is:", number3)
