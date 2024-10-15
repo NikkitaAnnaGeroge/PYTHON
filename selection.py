b76d4def644b3b8b22e751c250163c472a7491f2
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-10-15 14:10:09 +0530

    Add files via upload

---
+++ selection.py
@@ -0,0 +1,9 @@
+"""
+python program to check whether the given number
+is positive or not
+"""
+number=int(input("Enter a number:"))
+if number>0:
+    print("The given number:",number," is positive")
+else:
+    print("The given number:",number,"is negative")
