d23b80180a7912fea74c79a79e06fcede2b956b0
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-10-15 15:12:12 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age:"))
+if age<10:
+    print("The ticket fee for the person is 7")
+elif 10<=age<=60:
+    print("The ticket fee for the person is 10")
+else:
+    print("The ticket fee for the person is 5")
+
+
+
