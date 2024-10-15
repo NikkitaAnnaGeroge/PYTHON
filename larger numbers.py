872be30fbac5aaff0286772082a9b2fb38e34b3b
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-10-15 14:35:32 +0530

    Add files via upload

---
+++ larger numbers.py
@@ -0,0 +1,13 @@
+""""
+The program is to check the larger of two numbers
+author:nikkitaanna
+created on:15/10/24
+
+number1=int(input("Enter your first number:"))
+number2=int(input("Enter your second number:"))
+if number1>number2:
+    print("The greater the number is",number1)
+elif number1<number2:
+    print("The greater the number is", number2)
+else:
+    print("numbers are equal")
