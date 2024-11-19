2e20faeacc11c1a0b497cf1920e7a3a69ec5e329
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-19 13:43:21 +0530

    Add files via upload

---
+++ tupplr.py
@@ -0,0 +1,27 @@
+fruits=(["apple","cherry","banana","pomegranate"])
+length=len(fruits)
+print(length)
+
+tuple1=(["banana","orange"])
+tuple2=(["apple","pomegranate"])
+tuple3=(tuple1+tuple2)
+print(tuple3)
+
+repeat_tuple=('fruits',)*3
+print(repeat_tuple)
+
+colors=('blue','red','orange','yellow')
+tuple1=colors[0:3]
+print(tuple1)
+tuple2=colors[-2:]
+print(tuple2)
+
+person=("Alice",5,"engineer")
+name,age,profession=person
+print(name)
+
+inventory=[
+    ('Laptop',1000,)
+
+
+
